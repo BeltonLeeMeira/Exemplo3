0c71c225729f333c5b6e66ccf2c612ed8e8db456
Belton <blcdmmeira@gmail.com>
2024-10-03 11:25:41 -0300

    Criando uma funcao

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 //ESTRUTURA DE REPETIÇÃO
 
 for (let i=0; i<10;i++){
-    console.log(i)
+    // console.log(i)
 }
 
 //WHILE
 
 let f =0;
 while (f <10){
-    console.log(f)
+    // console.log(f)
     f++;
 }
+
+//DO WHILE
+let w=0;
+
+do{
+    // console.log(w);
+    w++;
+}
+while(w5);
+
+//FUNÇÕES
+
+function Ola(faculdade){
+    console.log(`Seja bem vindo a ${faculdade}`)
+    alert(`Seja bem vindo a ${faculdade}`)
+    write(`Seja bem vindo a ${faculdade}`)
+}
+//EXECUTANDO A FUNÇÃO OLA PASSANDO O VALOR
+Ola("fiap")
